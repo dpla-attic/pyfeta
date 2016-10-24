1f51a8d3e0991271dbc41c05efaa7a37b1ec69e8
Mark Breedlove <mb@dp.la>
2016-10-24 09:30:09 -0400

    Refactor multiprocessing_mapping.py
    * Remove print statements
    * Use os.scandir for listing files
    * Don't assign asynchronous subprocesses to an array
    * Use `with` for Pool
    * Use exception callback for `Pool.apply_async`

--- multiprocessing_mapping.py
+++ multiprocessing_mapping.py
@@ -1,35 +1,38 @@
 from multiprocessing import *
-from os import listdir
+from os import scandir
 from os.path import isfile, join
 from cdl_map import CdlMap
 import sys
 
 
-def handle_file(in_path, out_path, file):
-    # print(file)
-    with open(join(in_path, file), 'r') as in_file:
-        try:
-            output = CdlMap.map(in_file)
-            with open(join(out_path, file), 'w') as out_file:
-                out_file.write(output.decode('utf8'))
+def handle_file(in_path, out_path, filename):
+    with open(join(in_path, filename), 'r') as in_file:
+        output = CdlMap.map(in_file)
+        with open(join(out_path, filename), 'w') as out_file:
+            out_file.write(output.decode('utf8'))
+        return None
 
-            return "SUCCESS: " + file
-
-        except:
-            with open(join(out_path, file), 'w') as out_file:
-                out_file.write(str(sys.exc_info()[0]))
-
-            return "FAILURE: " + file
+def handle_error(e):
+    """Exception handler for multiprocessing.Pool.apply_async"""
+    # would log the exception here and increment a counter
+    print("caught exception: ", e)
 
 if __name__ == "__main__":
 
     if len(sys.argv) != 3:
-        print("Usage: multiprocessing_mapping <IN_DIR> <OUT_DIR>", file=sys.stderr)
+        print("Usage: multiprocessing_mapping <IN_DIR> <OUT_DIR>",
+              file=sys.stderr)
         exit(-1)
 
-    pool = Pool(processes=4)
-    files = [pool.apply_async(handle_file, (sys.argv[1], sys.argv[2], f)) for f in listdir(sys.argv[1]) if isfile(join(sys.argv[1], f))]
-    for res in files:
-        print(res.get(timeout=1))
+    fcount = 0
+    with Pool(processes=4) as pool:
+        for f in scandir(sys.argv[1]):
+            fcount += 1
+            if f.is_file():
+                pool.apply_async(handle_file,
+                                 args=(sys.argv[1], sys.argv[2], f.name),
+                                 error_callback=handle_error)
+        pool.close()
+        pool.join()
 
-
+    print("Considered %d files" % fcount)
